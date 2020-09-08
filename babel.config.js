78f1ecc27b845f68e0f84dda839b4887ddb0f34f
Adrian Kirsten <adrian@wealthmigrate.com>
2020-09-08 14:24:37 +0200

    system: configuring styled components

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   comments: false,
-  plugins: ['babel-plugin-styled-components'],
+  plugins: [
+    [
+      'babel-plugin-styled-components',
+      {
+        "namespace": "traaidmark-konstruct",
+        "minify": true,
+        "transpileTemplateLiterals": true
+      }
+    ]
+  ],
   presets: ['@babel/preset-env', '@babel/preset-react'],
 };
