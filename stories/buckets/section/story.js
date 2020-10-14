343a1860b891c2eda4ae057f038000b23b97d984
Adrian Kirsten <adrian@wealthmigrate.com>
2020-10-14 09:03:48 +0200

    documentation: write docs for Section component

---
+++ stories/buckets/section/story.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Foundation from '../../../packages/foundation/src';
+import { Section } from '../../../packages/buckets/src';
+import mdx from './doc.mdx';
+export default {
+  title: 'Buckets/Section',
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+  },
+  component: Section,
+};
+export const Demo = (args) => (
+  <Foundation>
+    <Section { ...args }>
+      <p>Sed tristique malesuada arcu dignissim molestie. Maecenas interdum accumsan risus. In finibus lorem massa, eu accumsan tortor imperdiet nec. Proin dictum malesuada elit, varius euismod urna aliquet nec. Ut ornare libero nisl, in imperdiet arcu molestie non. Curabitur vel orci placerat, molestie nisl eget, lacinia felis. Sed malesuada, diam sit amet convallis semper, mi neque ullamcorper enim, a porttitor velit dui eget leo. Ut laoreet nibh vitae bibendum tristique. Vestibulum non tempus purus, nec scelerisque magna. Nulla leo odio, ullamcorper vel enim sed, maximus pretium sapien. Duis a augue pharetra ex consequat ultrices. Vivamus odio ligula, consequat non turpis ac, pretium posuere est.</p>
+    </Section>
+  </Foundation>
+);
+
+Demo.args = {
+  headerText: 'This is my Heading!'
+};
