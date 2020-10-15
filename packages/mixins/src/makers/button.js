2d6d5674f040659cd189606f9f298ee8b4aa371c
Adrian Kirsten <adrian@wealthmigrate.com>
2020-10-15 08:16:29 +0200

    bugfix: referencing wrong variables

--- packages/mixins/src/makers/button.js
+++ packages/mixins/src/makers/button.js
@@ -14,29 +14,28 @@
 
 // 2.1. MAKE BUTTON ............................................................
 
-const makeBtn = (v, s) => {
+const makeBtn = (v) => {
   if (v.includes('icon')) {
     return `
-    
       border-radius: 100%;
     
       svg > path {
-        fill: var(--btn-${ variant }--color);
+        fill: var(--btn-${ v }--color);
         transition: var(--transition-fast);
       }
     
       &.active {
-        background-color: var(--btn-${ variant }--bgA);
-        border-color: var(--btn-${ variant }--borderA);
+        background-color: var(--btn-${ v }--bgA);
+        border-color: var(--btn-${ v }--borderA);
     
         svg > path {
-          fill: var(--btn-${ variant }--colorA);
+          fill: var(--btn-${ v }--colorA);
         }
       }
     
       &:hover {
         svg > path {
-          fill: var(--btn-${ variant }--colorH);
+          fill: var(--btn-${ v }--colorH);
         }
       }
     `;
@@ -46,7 +45,7 @@ const makeBtn = (v, s) => {
 
     width: fit-content;
 
-    color: var(--btn-${ variant }--color)!important;
+    color: var(--btn-${ v }--color)!important;
 
     font-weight: var(--weight-bold);
     font-family: var(--family-secondary);
@@ -54,7 +53,7 @@ const makeBtn = (v, s) => {
     border-radius: var(--radius-small);
 
     &:hover {
-      color: var(--btn-${ variant }--colorH)!important;
+      color: var(--btn-${ v }--colorH)!important;
     }
   `;
 };
@@ -66,44 +65,49 @@ const makeBtn = (v, s) => {
 // 2. STYLESHEET +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 export default (
-  variant = 'primary',
-) => `
+  variant = undefined,
+) => {
+  if (variant === undefined) {
+    throw new Error('_makeButton: Parameters are missing or incomplete.');
+  };
+  return `
 
-  ${ makeBtn(variant) }
+    ${ variant && makeBtn(variant) }
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-  background-color: var(--btn-${ variant }--bg);
-  border: solid 2px var(--btn-${ variant }--border);
+    background-color: var(--btn-${ variant }--bg);
+    border: solid 2px var(--btn-${ variant }--border);
 
-  box-shadow: none;
-  transform: translateY(0) scale(1);
-  transition: var(--transition-fast);
+    box-shadow: none;
+    transform: translateY(0) scale(1);
+    transition: var(--transition-fast);
 
-  &:hover {
-    
-    cursor: pointer;
-
-    background-color: var(--btn-${ variant }--bgH);
-    border-color: var(--btn-${ variant }--borderH);
-
-    box-shadow: 1px 1px 10px rgba(0,0,0, 0.3);
-    transform: translateY(-2px) scale(1.02);
-
-  }
-
-  &:disabled {
-    opacity: 0.8;
     &:hover {
-      cursor: not-allowed;
-      box-shadow: none;
-      transform: translateY(0px) scale(1);
-    }
-  }
+      
+      cursor: pointer;
 
-`;
+      background-color: var(--btn-${ variant }--bgH);
+      border-color: var(--btn-${ variant }--borderH);
+
+      box-shadow: 1px 1px 10px rgba(0,0,0, 0.3);
+      transform: translateY(-2px) scale(1.02);
+
+    }
+
+    &:disabled {
+      opacity: 0.8;
+      &:hover {
+        cursor: not-allowed;
+        box-shadow: none;
+        transform: translateY(0px) scale(1);
+      }
+    }
+
+  `
+};
 
 // 2. END ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
