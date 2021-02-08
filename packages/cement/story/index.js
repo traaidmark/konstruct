2c8b7699c313a5d2aafb2bbb8f8196a0c546d2e5
Adrian Kirsten <adrian@wealthmigrate.com>
2021-02-08 09:12:25 +0200

    refactor: remove dead code

--- packages/cement/story/index.js
+++ packages/cement/story/index.js
@@ -16,9 +16,22 @@ import mdx from './doc.mdx';
 
 import '../src/core.css';
 
+import '../src/part-button.css';
+import '../src/part-flag.css';
+import '../src/part-data.css';
+import '../src/part-sticker.css';
+
+import '../src/bucket-section.css';
+import '../src/bucket-grid.css';
+import '../src/bucket-block.css';
+
+import '../src/utility-list.css';
+
 
 import Section from '../../buckets/section/src';
 import Grid from '../../buckets/grid/src';
+
+import List from '../../../storybook-components/List';
 
 // 1.2. END ....................................................................
 
@@ -258,6 +271,498 @@ export const Core = (args) => (
 );
 
 // 3.1. END ....................................................................
+
+// 3.2. PARTS ..................................................................
+
+export const Parts = (args) => (
+  <>
+    <Section noLeft noRight noTop>
+      <h2>Buttons</h2>
+      <List>
+        <li>
+          <a href="#" className="p-button">Primary Button</a>
+        </li>
+        <li>
+          <a href="#" className="p-button p-button--danger">Dangerous Action</a>
+        </li>
+        <li>
+          <a href="#" className="p-button p-button--warning">Careful Action</a>
+        </li>
+        <li>
+          <a href="#" className="p-button p-button--safe">Safe Action</a>
+        </li>
+      </List>
+      <h4>Code</h4>
+      <Source
+        code={dedent`
+        <a href="#" className="p-button">Button</a>
+        <a href="#" className="p-button p-button--danger">Button</a>
+        <a href="#" className="p-button p-button--warning">Button</a>
+        <a href="#" className="p-button p-button--safe">Button</a>
+        `}
+      />
+    </Section>
+    <Section noLeft noRight noTop>
+      <h3>Button Sizes</h3>
+      <List>
+        <li>
+          <a href="#" className="p-button p-button--small">Small Button</a>
+        </li>
+        <li>
+          <a href="#" className="p-button p-button--primary">Default Button</a>
+        </li>
+        <li>
+          <a href="#" className="p-button p-button--large">Large Button</a>
+        </li>
+      </List>
+      
+      <h4>Code</h4>
+      <Source
+        code={dedent`
+        <a href="#" className="p-button p-button--small">Button</a>
+        <a href="#" className="p-button p-button--large">Button</a>
+        `}
+      />
+    </Section>
+    <Section noLeft noRight noTop>
+      <h3>Full width button</h3>
+      <a href="#" className="p-button p-button--full">Full width Button</a>
+      <br />
+      
+      <h4>Code</h4>
+      <Source
+        code={dedent`
+        <a href="#" className="p-button p-button--full">Button</a>
+        `}
+      />
+    </Section>
+    <Section noLeft noRight noTop headerText="Flags">
+      <List>
+        <li>
+          <span className="p-flag">flag</span>
+        </li>
+        <li>
+          <span className="p-flag p-flag--info">info</span>
+        </li>
+        <li>
+          <span className="p-flag p-flag--new">new</span>
+        </li>
+        <li>
+          <span className="p-flag p-flag--aware">aware</span>
+        </li>
+        <li>
+          <span className="p-flag p-flag--important">important</span>
+        </li>
+      </List>
+      <h4>Code</h4>
+      <Source
+        code={dedent`
+        <span className="p-flag">flag</span>
+        <span className="p-flag p-flag--info">info</span>
+        <span className="p-flag p-flag--new">new</span>
+        <span className="p-flag p-flag--aware">aware</span>
+        <span className="p-flag p-flag--important">important</span>
+        `}
+      />
+    </Section>
+    <Section noLeft noRight noTop>
+      <List>
+        <li>
+          <div className="p-data">
+            <i>USD</i>
+            <strong>300.00</strong>
+          </div>
+        </li>
+      </List>
+      <h4>Code</h4>
+      <Source
+        code={dedent`
+        <div className="p-data">
+          <i>USD</i>
+          <strong>300.00</strong>
+        </div>
+        `}
+      />
+    </Section>
+    <Section noLeft noRight noTop headerText="Data">
+      <h3>Size Variations</h3>
+      <Grid>
+        <aside>
+          <h5><code>part-data--small</code></h5>
+          <div className="p-data p-data--small">
+            <i>USD</i>
+            <strong>300.00</strong>
+          </div>
+          <Source
+            code={dedent`
+            <div className="p-data p-data--small">
+              <i>USD</i>
+              <strong>300.00</strong>
+            </div>
+            `}
+          />
+        </aside>
+        <aside>
+          <h5><code>part-data--large</code></h5>
+          <div className="p-data p-data--large">
+            <i>USD</i>
+            <strong>300.00</strong>
+          </div>
+          <Source
+            code={dedent`
+            <div className="p-data p-data--large">
+              <i>USD</i>
+              <strong>300.00</strong>
+            </div>
+            `}
+          />
+        </aside>
+        <aside>
+          <h5><code>part-data--xlarge</code></h5>
+          <div className="p-data p-data--xlarge">
+            <i>USD</i>
+            <strong>300.00</strong>
+          </div>
+          <Source
+            code={dedent`
+            <div className="p-data p-data--xlarge">
+              <i>USD</i>
+              <strong>300.00</strong>
+            </div>
+            `}
+          />
+        </aside>
+      </Grid>
+    </Section>
+
+    <Section noLeft noRight noTop headerText="Sticker">
+      <List>
+        <li>
+          <div className="p-sticker">
+            <small>Sticker Label</small>
+            <div className="p-data">
+              <i>USD</i>
+              <strong>300.00</strong>
+            </div>
+          </div>
+        </li>
+      </List>
+      <h4>Code</h4>
+      <Source
+        code={dedent`
+        <div className="p-sticker">
+          <small>Sticker Label</small>
+          <div className="p-data">
+            <i>USD</i>
+            <strong>300.00</strong>
+          </div>
+        </div>
+        `}
+      />
+    </Section>
+    <Section noLeft noRight noTop>
+      <h3>Vertical Orientation</h3>
+      <Grid>
+        <aside>
+          <h5><code>part-sticker--top</code></h5>
+          <div className="p-sticker p-sticker--top">
+            <small>Sticker Label</small>
+            <div className="p-data">
+              <i>USD</i>
+              <strong>300.00</strong>
+            </div>
+          </div>
+          <Source
+            code={dedent`
+            <div className="p-sticker p-sticker--top">
+              <small>Sticker Label</small>
+              <div className="p-data">
+                <i>USD</i>
+                <strong>300.00</strong>
+              </div>
+            </div>
+            `}
+          />
+        </aside>
+        <aside>
+          <h5><code>part-sticker--bottom</code></h5>
+          <div className="p-sticker p-sticker--bottom">
+            <small>Sticker Label</small>
+            <div className="p-data">
+              <i>USD</i>
+              <strong>300.00</strong>
+            </div>
+          </div>
+          <Source
+            code={dedent`
+            <div className="p-sticker p-data--bottom">
+              <small>Sticker Label</small>
+              <div className="p-data">
+                <i>USD</i>
+                <strong>300.00</strong>
+              </div>
+            </div>
+            `}
+          />
+        </aside>
+      </Grid>
+    </Section>
+    <Section noLeft noRight noTop>
+      <h3>Horizontal Orientation</h3>
+      <Grid>
+        <aside>
+          <h5><code>part-sticker--left</code></h5>
+          <div className="p-sticker p-sticker--left">
+            <small>Sticker Label</small>
+            <div className="p-data">
+              <i>USD</i>
+              <strong>300.00</strong>
+            </div>
+          </div>
+          <Source
+            code={dedent`
+            <div className="p-sticker p-sticker--left">
+              <small>Sticker Label</small>
+              <div className="p-data">
+                <i>USD</i>
+                <strong>300.00</strong>
+              </div>
+            </div>
+            `}
+          />
+        </aside>
+        <aside>
+          <h5><code>part-sticker--center</code></h5>
+          <div className="p-sticker p-sticker--center">
+            <small>Sticker Label</small>
+            <div className="p-data">
+              <i>USD</i>
+              <strong>300.00</strong>
+            </div>
+          </div>
+          <Source
+            code={dedent`
+            <div className="p-sticker p-data--center">
+              <small>Sticker Label</small>
+              <div className="p-data">
+                <i>USD</i>
+                <strong>300.00</strong>
+              </div>
+            </div>
+            `}
+          />
+        </aside>
+        <aside>
+          <h5><code>part-sticker--right</code></h5>
+          <div className="p-sticker p-sticker--right">
+            <small>Sticker Label</small>
+            <div className="p-data">
+              <i>USD</i>
+              <strong>300.00</strong>
+            </div>
+          </div>
+          <Source
+            code={dedent`
+            <div className="p-sticker p-data--right">
+              <small>Sticker Label</small>
+              <div className="p-data">
+                <i>USD</i>
+                <strong>300.00</strong>
+              </div>
+            </div>
+            `}
+          />
+        </aside>
+      </Grid>
+    </Section>
+
+  </>
+);
+
+
+// 3.2. END ....................................................................
+
+// 3.3. BUCKETS ................................................................
+
+export const Buckets = (args) => (
+  <>
+    <Section  noLeft noRight noTop headerTitle="Section">
+      <section className="b-section">
+        <header className="b-section__header">
+          <h2>Default Section</h2>
+          <p>Nulla leo odio, ullamcorper vel enim sed, maximus pretium sapien. Duis a augue pharetra ex consequat ultrices. Vivamus odio ligula, consequat non turpis ac, pretium posuere est.</p>
+        </header>
+        <p>Sed tristique malesuada arcu dignissim molestie. Maecenas interdum accumsan risus. In finibus lorem massa, eu accumsan tortor imperdiet nec. Proin dictum malesuada elit, varius euismod urna aliquet nec. Ut ornare libero nisl, in imperdiet arcu molestie non. Curabitur vel orci placerat, molestie nisl eget, lacinia felis. Sed malesuada, diam sit amet convallis semper, mi neque ullamcorper enim, a porttitor velit dui eget leo. Ut laoreet nibh vitae bibendum tristique. Vestibulum non tempus purus, nec scelerisque magna. Nulla leo odio, ullamcorper vel enim sed, maximus pretium sapien. Duis a augue pharetra ex consequat ultrices. Vivamus odio ligula, consequat non turpis ac, pretium posuere est.</p>
+        <footer className="b-section__footer">
+          <h3>This is a call to action header</h3>
+          <ul class="u-list-horizontal u-list-horizontal--center">
+            <li><a href="">Action Link</a></li>
+            <li><a href="">Action Link</a></li>
+          </ul>
+        </footer>
+      </section>
+
+      <h4>Code</h4>
+      <Source
+        code={dedent`
+        <section className="b-section">
+          <header className="b-section__header">
+            <h2>Default Section</h2>
+            <p>Nulla leo odio, ullamcorper vel enim sed, maximus pretium sapien. Duis a augue pharetra ex consequat ultrices. Vivamus odio ligula, consequat non turpis ac, pretium posuere est.</p>
+          </header>
+          <p>Sed tristique malesuada arcu dignissim molestie. Maecenas interdum accumsan risus. In finibus lorem massa, eu accumsan tortor imperdiet nec. Proin dictum malesuada elit, varius euismod urna aliquet nec. Ut ornare libero nisl, in imperdiet arcu molestie non. Curabitur vel orci placerat, molestie nisl eget, lacinia felis. Sed malesuada, diam sit amet convallis semper, mi neque ullamcorper enim, a porttitor velit dui eget leo. Ut laoreet nibh vitae bibendum tristique. Vestibulum non tempus purus, nec scelerisque magna. Nulla leo odio, ullamcorper vel enim sed, maximus pretium sapien. Duis a augue pharetra ex consequat ultrices. Vivamus odio ligula, consequat non turpis ac, pretium posuere est.</p>
+          <footer className="b-section__footer">
+            <h3>This is a call to action header</h3>
+            <ul class="u-list-horizontal u-list-horizontal--center">
+              <li><a href="">Action Link</a></li>
+              <li><a href="">Action Link</a></li>
+            </ul>
+          </footer>
+        </section>
+        `}
+      />
+    </Section>
+  </>
+);
+
+// 3.3. END ....................................................................
+
+// 3.4. UTILITIES ..............................................................
+
+export const Utilities = (args) => (
+  <>
+    <Section noLeft noRight noTop headerText="Lists">
+      <h3>Vertical List</h3>
+      <ul class="u-list-vertical">
+        <li>
+          <a href="#">Link in a list</a>
+        </li>
+        <li>
+          <a href="#">Link in a list</a>
+        </li>
+        <li>
+          <a href="#">Link in a list</a>
+        </li>
+        <li>
+          <a href="#">Link in a list</a>
+        </li>
+        <li>
+          <a href="#">Link in a list</a>
+        </li>
+        <li>
+          <a href="#">Link in a list</a>
+        </li>
+      </ul>
+      <h4>Code</h4>
+      <Source
+        code={dedent`
+        <ul class="u-list-vertical">
+        ...
+        </ul>
+        `}
+      />
+    </Section>
+    <Section noLeft noRight noTop>
+      <h3>Horizontal List</h3>
+      <ul class="u-list-horizontal">
+        <li>
+          <a href="#">Link in a list</a>
+        </li>
+        <li>
+          <a href="#">Link in a list</a>
+        </li>
+        <li>
+          <a href="#">Link in a list</a>
+        </li>
+        <li>
+          <a href="#">Link in a list</a>
+        </li>
+        <li>
+          <a href="#">Link in a list</a>
+        </li>
+        <li>
+          <a href="#">Link in a list</a>
+        </li>
+      </ul>
+      <h4>Code</h4>
+      <Source
+        code={dedent`
+        <ul class="u-list-horizontal">
+        ...
+        </ul>
+        `}
+      />
+    </Section>
+    <Section noLeft noRight noTop>
+      <Grid>
+        <aside>
+          <h3>Horizontal List - Divided</h3>
+          <ul class="u-list-horizontal u-list--divided">
+            <li>
+              <a href="#">Link in a list</a>
+            </li>
+            <li>
+              <a href="#">Link in a list</a>
+            </li>
+            <li>
+              <a href="#">Link in a list</a>
+            </li>
+            <li>
+              <a href="#">Link in a list</a>
+            </li>
+            <li>
+              <a href="#">Link in a list</a>
+            </li>
+            <li>
+              <a href="#">Link in a list</a>
+            </li>
+          </ul>
+          <h4>Code</h4>
+          <Source
+            code={dedent`
+            <ul class="u-list-horizontal u-list--divided">
+            ...
+            </ul>
+            `}
+          />
+        </aside>
+        <aside>
+          <h3>Vertical List - Underlined</h3>
+          <ul class="u-list-vertical u-list--underlined">
+            <li>
+              <a href="#">Link in a list</a>
+            </li>
+            <li>
+              <a href="#">Link in a list</a>
+            </li>
+            <li>
+              <a href="#">Link in a list</a>
+            </li>
+            <li>
+              <a href="#">Link in a list</a>
+            </li>
+            <li>
+              <a href="#">Link in a list</a>
+            </li>
+            <li>
+              <a href="#">Link in a list</a>
+            </li>
+          </ul>
+          <h4>Code</h4>
+          <Source
+            code={dedent`
+            <ul class="u-list-vertical">
+            ...
+            </ul>
+            `}
+          />
+        </aside>
+      </Grid>
+    </Section>
+  </>
+);
+
+// 3.4. END ....................................................................
 
 // 3. END ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
