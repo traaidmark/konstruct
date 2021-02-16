04937d052a1293b4ba24269614c4181ccdc1b2ca
Adrian Kirsten <adrian@wealthmigrate.com>
2021-02-16 12:17:52 +0200

    feature: Add data back in

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -16,6 +16,7 @@ export const parameters = {
         [
           'Card',
           'Flag',
+          'Data',
         ]  
       ]
     }
--- packages/parts/data/README.md
+++ packages/parts/data/README.md
@@ -1,4 +1,4 @@
-# Konstruct > Parts > Flag
+# Konstruct > Parts > Data
 
 ## Documentation
 
